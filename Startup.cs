17ba6aadc80ce8661c42c2aaa11bbe3a12aa485b
Cade Jorgensen <cade25k@byu.edu>
2022-03-09 11:29:25 -0700

    mission 10

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace Mission7
             services.AddSession();
             services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,6 +76,8 @@ namespace Mission7
 
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 
                 //endpoints.MapControllerRoute(
                   //  name: "default",
